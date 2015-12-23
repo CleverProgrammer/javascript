b3efecd9f4d6b4cc52673d70baab86212b9baade
rafeh01 <rafehqazi1@gmail.com>
2015-12-22 19:45:25 -0600

    added first javascript program. Problem from free_code_camp under intermediate Javascript algorithms.

---
+++ freeCodeCamp/union.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Rafeh on 12/22/15.
+ * Program that creates a union of lists and returns them after omitting duplicates.
+ */
+
+
+function if_in(num_list, search_num) {
+    for (var i in num_list) {
+        if (num_list[i] == search_num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function unite(arr1, arr2, arr3) {
+    var united = arr1;
+    for (var i in arr2) {
+        if (!(if_in(united, arr2[i]))) {
+            united.push(arr2[i]);
+        }
+    }
+    for (var i in arr3) {
+        if (!(if_in(united, arr3[i]))) {
+            united.push(arr3[i]);
+        }
+    }
+    return united;
+}
+
+console.log(unite([1, 2, 3], [2, 2, 2], [5, 6, 7]));
