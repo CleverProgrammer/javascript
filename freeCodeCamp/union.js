64a7c6a5da3a069d771d8e88c60e3da6e1b4756e
rafeh01 <rafehqazi1@gmail.com>
2015-12-24 20:37:58 -0600

    removed unit test. Also, converted for loops to forEach loops.

--- freeCodeCamp/union.js
+++ freeCodeCamp/union.js
@@ -15,22 +15,24 @@ function if_in(num_list, search_num) {
 
 // > unite([1, 2, 3], [2, 2, 2], [5, 6, 7])
 // [1, 2, 3, 5, 6, 7]
-// > unite([1, 2, 2], [2, 2, 2], [5, 6, 7])
-// [1, 2, 5, 6, 7]
+// > unite([1, 2, 3], [2, 2, 2], [5, 6, 7])
+// [1, 2, 3, 5, 6, 7]
 function unite(arr1, arr2, arr3) {
     var united = arr1;
-    for (var i in arr2) {
-        if (!(if_in(united, arr2[i]))) {
-            united.push(arr2[i]);
+    arr2.forEach(function (element) {
+        if (!(if_in(united, element))) {
+            united.push(element);
         }
-    }
-    for (var i in arr3) {
-        if (!(if_in(united, arr3[i]))) {
-            united.push(arr3[i]);
+    });
+    arr3.forEach(function (element) {
+        if (!(if_in(united, element))) {
+            united.push(element);
         }
-    }
+    });
     return united;
 }
+
+console.log(unite([1, 2, 3], [2, 2, 2], [5, 6, 7]));
 
 // console.log(unite([1, 2, 3], [2, 2, 2], [5, 6, 7]));
 // var result = unite([1, 2, 3], [2, 2, 2], [5, 6, 7]);
