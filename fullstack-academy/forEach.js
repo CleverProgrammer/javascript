d2eca90e7ce1b1562df2396826de6e53300810d0
rafeh01 <rafehqazi1@gmail.com>
2016-06-15 00:36:10 -0500

    Complete forEach

---
+++ fullstack-academy/forEach.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by ChessTastic on 6/15/16.
+ */
+
+function double(arr) {
+  var doubled = [];
+  arr.forEach(function(item) {
+    doubled.push(item * 2);
+  });
+  return doubled;
+}
